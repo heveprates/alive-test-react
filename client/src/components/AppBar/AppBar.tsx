0ee7051e79d88a7e201a5b5150b34aa8c8ac886a
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-07 17:31:34 -0300

    feat: criacao do component AppBar

---
+++ client/src/components/AppBar/AppBar.tsx
@@ -0,0 +1,35 @@
+import AppBarMaterial from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { Search, SearchIconWrapper, StyledInputBase } from './styles';
+
+export default function AppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBarMaterial>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            TESTE HEVELLYN ALPHA
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBarMaterial>
+    </Box>
+  );
+}
