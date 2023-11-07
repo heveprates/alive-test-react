a1f96244950309490601c295e1bef587578d36fc
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-07 19:53:55 -0300

    feat: utilizado a dashboard na home

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,14 +1,28 @@
-import { Container } from '@mui/material';
-import AppBar from '../AppBar/AppBar';
-import History from '../History';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import AppBar from '../AppBar';
+import Dashboard from '../Dashboard/Dashboard';
 
 export default function Home() {
   return (
     <>
       <AppBar />
-      <Container>
-        <History />
-      </Container>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+          <Dashboard />
+        </Container>
+      </Box>
     </>
   );
 }
