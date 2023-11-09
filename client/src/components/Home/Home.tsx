40a9471740f22453e455e8448c4ce1b31993d2ca
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-08 23:20:42 -0300

    feat: usuando o store na Home para exibir a Dashboard

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,11 +1,25 @@
+import { Box, Typography } from '@mui/material';
+
 import MainLayout from '../../Layout/MainLayout';
+import { useQuote } from '../../stores/useQuote';
 import Dashboard from '../Dashboard';
 
 export default function Home() {
+  const stock = useQuote((state) => state.stock);
+  const hasStock = stock !== null;
   return (
     <>
       <MainLayout>
-        <Dashboard />
+        {hasStock ? (
+          <Dashboard />
+        ) : (
+          <Box mt={8} textAlign="center">
+            <Typography variant="h3" component="h1">
+              Busque por conhecimento
+            </Typography>
+            <Typography variant="h5">ET Bilu</Typography>
+          </Box>
+        )}
       </MainLayout>
     </>
   );
