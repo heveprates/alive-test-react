64fe7feb99153d1eae1d1933730d986939fb9cd1
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-09 15:41:08 -0300

    feat: alterando titulo do projeto

--- client/src/components/AppBar/AppBar.tsx
+++ client/src/components/AppBar/AppBar.tsx
@@ -28,7 +28,7 @@ export default function AppBar() {
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-            TESTE HEVELLYN ALPHA
+            TESTE HEVELLYN
           </Typography>
           <form onSubmit={handleSearchSubmit}>
             <Search>
