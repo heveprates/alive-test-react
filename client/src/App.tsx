660070ea1e97c7d149c7ae4b851876a67e72bfea
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-09 09:10:47 -0300

    feat: limpando store de gain-loss quando muda stock

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { LocalizationProvider } from '@mui/x-date-pickers';
@@ -6,11 +7,25 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/pt-br';
 
 import Home from './components/Home';
+import { useQuote } from './stores/useQuote';
+import { useGainLoss } from './stores/useGainLoss';
 
 dayjs.locale('pt-br');
 const defaultTheme = createTheme();
 
+const clearStore = {
+  handle: () => {},
+};
+
 function App() {
+  const stock = useQuote((state) => state.stock);
+  const clearGainLoss = useGainLoss((state) => state.clearGainLoss);
+  clearStore.handle = () => {
+    clearGainLoss();
+  };
+  useEffect(() => {
+    clearStore.handle();
+  }, [stock]);
   return (
     <ThemeProvider theme={defaultTheme}>
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
